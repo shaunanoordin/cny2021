8c17a50b255111e8f3f9575715fb159f56c73a18
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-02-02 02:10:54 +0000

    Add gravity well to Goal

--- src/cny2021/entities/goal.js
+++ src/cny2021/entities/goal.js
@@ -6,10 +6,26 @@ class Goal extends Entity {
     super(app)
     
     this.colour = '#cc4'
-    this.size = TILE_SIZE * 3
+    this.size = TILE_SIZE * 2
     this.solid = false
     this.x = col * TILE_SIZE + TILE_SIZE / 2
     this.y = row * TILE_SIZE + TILE_SIZE / 2
+  }
+  
+  onCollision (target, collisionCorrection) {
+    if (target !== this._app.hero) return null
+    
+    const goal = this
+    const hero = target
+    
+    // Pull hero to the centre
+    const distX = goal.x - hero.x
+    const distY = goal.y - hero.y
+    const angleToGoal = Math.atan2(distY, distX)
+    const distanceToGoal = Math.sqrt(distX * distX + distY * distY)
+    
+    hero.speedX = Math.cos(angleToGoal) * Math.min(distanceToGoal, hero.movementSpeed)
+    hero.speedY = Math.sin(angleToGoal) * Math.min(distanceToGoal, hero.movementSpeed)
   }
 }
   
